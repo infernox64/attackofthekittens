497bc5efa471577626f44193779f52bfcdd79cde
Matt Johnson <infernox64@gmail.com>
2025-04-13 22:29:44 -0400

    Update util.js

--- scripts/util.js
+++ scripts/util.js
@@ -1,7 +1,7 @@
 
 const imgCount = 13;
 function newImg(id) {
-    let imgsrc = `images/lol${id}.jpg`
+    let imgsrc = `images/lol0${id}.jpg`
     let imgEl = document.createElement('img');
 imgEl.src = imgsrc;
 imgEl.classList = "img";
